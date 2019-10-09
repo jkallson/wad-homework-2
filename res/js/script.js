e27e0b1836b827971a3be61d79b68ba397c928cb
jkallson <janar.kallson@gmail.com>
2019-10-09 23:27:14 +0300

    Created script.js and created new User and Course objects in it

---
+++ res/js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    var user = new User("Ahto","Leemet","28/10/1998","Car mechanic","4.5")
+
+    var courses = [
+        new Course("Agile software development","1","82"),
+        new Course("System modeling","1","85"),
+        new Course("Object-orientated programming","2","99"),
+        new Course("Estonian language Level A2","2","65")
+
+    ];
+});
