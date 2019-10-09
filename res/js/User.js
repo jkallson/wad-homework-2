a0b4d560700905b8074e7711a758f340d9538748
jkallson <janar.kallson@gmail.com>
2019-10-09 23:19:36 +0300

    Added Course and User classes

---
+++ res/js/User.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(firstname, lastname, birthdate, faculity, gpa){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.faculity = faculity;
+        this.gpa = gpa;
+    }
+}
