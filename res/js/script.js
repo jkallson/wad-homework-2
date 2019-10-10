d7396e0aae5b9bc298bacabb0fd1c85440eb063b
Anders Nõu <anders.nou@gmail.com>
2019-10-10 11:54:57 +0300

    Added GPA updating and calculating when adding a course See #4 and See #5 Close #5

--- res/js/script.js
+++ res/js/script.js
@@ -32,16 +32,16 @@ $(function () {
 
     $("#save-course").click(function () {
         let table = $("#courses");
-        let courseTitle = $("#title").val();
-        let semester = $("#semester").val();
-        let grade = $("#grade").val();
+        let courseTitle = $("#title");
+        let semester = $("#semester");
+        let grade = $("#grade");
         let lastId = $("#courses tr:last").find("td:first").html();
 
         let tr = $("<tr></tr>");
         let td1 = $("<td></td>").text(parseInt(lastId)+1);
-        let td2 = $("<td></td>").text(courseTitle);
-        let td3 = $("<td></td>").text(semester);
-        let td4 = $("<td></td>").text(grade);
+        let td2 = $("<td></td>").text(courseTitle.val());
+        let td3 = $("<td></td>").text(semester.val());
+        let td4 = $("<td></td>").text(grade.val());
 
         tr.append((td1));
         tr.append((td2));
@@ -50,14 +50,37 @@ $(function () {
 
         table.append(tr);
 
-        $("#title").val("");
-        $("#semester").val("");
-        $("#grade").val("");
+        courseTitle.val("");
+        semester.val("");
+        grade.val("");
 
         $("#add-course").toggle();
 
+        calculateGPA();
     });
 
+    function calculateGPA() {
+        let rowCount = $("#courses tr").length -1;
+        let pointSum = 0;
+        let gradePoint = 0;
+
+        $("#courses tr td:nth-child(4)").each(function() {
+            let gradeValue = $(this).text();
+            console.log(gradeValue);
+            if (gradeValue > 90)  gradePoint = 4;
+            else if (gradeValue > 80)  gradePoint = 3;
+            else if (gradeValue > 70)  gradePoint = 2;
+            else if (gradeValue > 60)  gradePoint = 1;
+            else if (gradeValue > 50)  gradePoint = 0.5;
+            else if (gradeValue <= 50)  gradePoint = 0;
+
+            console.log(gradePoint);
+            pointSum += parseFloat(gradePoint);
+        });
+
+        this.gpa = Math.round(pointSum/rowCount*100.0)/100.0;
+        $("#gpa strong").text(this.gpa);
+    }
     
     function init() {
         $("#name").text(user.firstname +" "+ user.lastname);
